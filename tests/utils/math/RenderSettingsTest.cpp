774d4f80bcfcaa33679657310e8543208ad1696f
LoannBi <loann.bideau@epitech.eu>
2026-04-27 18:59:26 +0200

    feat: implement RenderSettings class with default values and validation, and add unit tests

---
+++ tests/utils/math/RenderSettingsTest.cpp
@@ -0,0 +1,66 @@
+/*
+** EPITECH PROJECT, 2026
+** raytracer
+** File description:
+** RenderSettings unit tests
+*/
+
+#include <gtest/gtest.h>
+#include "Math/RenderSettings.hpp"
+
+using Math::RenderSettings;
+
+TEST(RenderSettingsTest, DefaultValues) {
+  RenderSettings s;
+  EXPECT_EQ(s.imageWidth, 800);
+  EXPECT_EQ(s.imageHeight, 600);
+  EXPECT_EQ(s.tileWidth, 32);
+  EXPECT_EQ(s.tileHeight, 32);
+  EXPECT_GT(s.numThreads, 0);
+  EXPECT_EQ(s.samplesPerPixel, 1);
+  EXPECT_EQ(s.maxDepth, 5);
+}
+
+TEST(RenderSettingsTest, ValidateDefaults) {
+  RenderSettings s;
+  EXPECT_TRUE(s.validate());
+}
+
+TEST(RenderSettingsTest, ValidateRejectsZeroWidth) {
+  RenderSettings s;
+  s.imageWidth = 0;
+  EXPECT_FALSE(s.validate());
+}
+
+TEST(RenderSettingsTest, ValidateRejectsZeroHeight) {
+  RenderSettings s;
+  s.imageHeight = 0;
+  EXPECT_FALSE(s.validate());
+}
+
+TEST(RenderSettingsTest, ValidateRejectsZeroSamples) {
+  RenderSettings s;
+  s.samplesPerPixel = 0;
+  EXPECT_FALSE(s.validate());
+}
+
+TEST(RenderSettingsTest, ValidateRejectsZeroDepth) {
+  RenderSettings s;
+  s.maxDepth = 0;
+  EXPECT_FALSE(s.validate());
+}
+
+TEST(RenderSettingsTest, ValidateRejectsNegativeValues) {
+  RenderSettings s;
+  s.tileWidth = -1;
+  EXPECT_FALSE(s.validate());
+}
+
+TEST(RenderSettingsTest, ValidateCustomValid) {
+  RenderSettings s;
+  s.imageWidth = 1920;
+  s.imageHeight = 1080;
+  s.samplesPerPixel = 64;
+  s.maxDepth = 10;
+  EXPECT_TRUE(s.validate());
+}
