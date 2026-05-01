644150468c6dca634d254e8fc159d35f3492b353
LoannBi <loann.bideau@epitech.eu>
2026-05-01 19:27:17 +0200

    feat(material): add IMaterial interface and implementation

---
+++ src/components/material/IMaterial.cpp
@@ -0,0 +1,12 @@
+/*
+** EPITECH PROJECT, 2026
+** raytracer
+** File description:
+** IMaterial
+*/
+
+#include "components/material/IMaterial.hpp"
+
+raytracer::math::Color IMaterial::emitted() const {
+  return {};
+}
