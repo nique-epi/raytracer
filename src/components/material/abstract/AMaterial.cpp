6b64507fd3a0d52110eed800a13dd71cd8764b7d
Loann Bideau <loann.bideau@epitech.eu>
2026-05-04 08:29:40 +0200

    feat(material): add IMaterial interface (#14)
    * feat(material): add IMaterial interface and implementation
    
    * fix(material): include Ray class in IMaterial header for proper type usage
    
    * fix(cmake): correct directory name from 'primitive' to 'Primitives' in CMakeLists
    
    * refactor(material): replace IMaterial with AMaterial as base class for non-emissive materials

---
+++ src/components/material/abstract/AMaterial.cpp
@@ -0,0 +1,12 @@
+/*
+** EPITECH PROJECT, 2026
+** raytracer
+** File description:
+** AMaterial
+*/
+
+#include "components/material/abstract/AMaterial.hpp"
+
+raytracer::math::Color AMaterial::emitted() const {
+  return {};
+}
