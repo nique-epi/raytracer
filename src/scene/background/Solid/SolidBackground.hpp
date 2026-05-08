622bb1626a65f8fa20130ec7d91888091f5d4bae
Yannislrg <yannis.lorang@epitech.eu>
2026-05-08 18:25:29 +0200

    feat(background): enhance IBackground and SolidBackground documentation

--- src/scene/background/Solid/SolidBackground.hpp
+++ src/scene/background/Solid/SolidBackground.hpp
@@ -12,8 +12,19 @@
 
 namespace raytracer::scene::background {
 
+/**
+ * @brief Background implementation that returns a single constant colour.
+ *
+ * This class represents a solid (uniform) background: every ray that misses
+ * the scene returns the same colour regardless of direction.
+ */
 class SolidBackground : public IBackground {
  public:
+  /**
+   * @brief Construct a solid background with given @p color.
+   *
+   * @param [in] color Colour to return for all rays.
+   */
   explicit SolidBackground(const raytracer::math::Color& color);
   ~SolidBackground() override = default;
 
@@ -22,6 +33,11 @@ class SolidBackground : public IBackground {
   SolidBackground(SolidBackground&&) = delete;
   SolidBackground& operator=(SolidBackground&&) = delete;
 
+  /**
+   * @brief Return the stored solid colour for @p ray.
+   *
+   * @inheritDoc
+   */
   [[nodiscard]] raytracer::math::Color getColor(
       const raytracer::math::Ray& ray) const override;
 
