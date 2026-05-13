1913f7a13128a40f894eece4325f2495948720b8
LoannBi <loann.bideau@epitech.eu>
2026-05-13 16:05:57 +0200

    refactor: remove redundant comments from BVH unit tests

--- tests/components/Primitives/BVHTest.cpp
+++ tests/components/Primitives/BVHTest.cpp
@@ -16,7 +16,6 @@
 using namespace raytracer::math;
 using raytracer::components::BVHNode;
 
-// Single object: BVH wraps it transparently
 TEST(BVHTest, SingleObjectHit) {
   std::vector<std::shared_ptr<IObject>> objects;
   objects.push_back(std::make_shared<SphereFixture>(Vector3D(0, 0, -2), 0.5));
@@ -29,7 +28,6 @@ TEST(BVHTest, SingleObjectHit) {
   EXPECT_NEAR(rec.t, 1.5, 1e-6);
 }
 
-// Single object: miss
 TEST(BVHTest, SingleObjectMiss) {
   std::vector<std::shared_ptr<IObject>> objects;
   objects.push_back(std::make_shared<SphereFixture>(Vector3D(5, 0, -2), 0.5));
@@ -41,7 +39,6 @@ TEST(BVHTest, SingleObjectMiss) {
   EXPECT_FALSE(bvh.hits(ray, 0.001, 1000.0, rec));
 }
 
-// Two objects: picks the nearest
 TEST(BVHTest, TwoObjects_PicksNearest) {
   std::vector<std::shared_ptr<IObject>> objects;
   objects.push_back(std::make_shared<SphereFixture>(Vector3D(0, 0, -3), 0.5));
@@ -55,7 +52,6 @@ TEST(BVHTest, TwoObjects_PicksNearest) {
   EXPECT_NEAR(rec.t, 0.5, 1e-6);
 }
 
-// Five objects along Z: BVH finds the nearest correctly
 TEST(BVHTest, ManyObjects_PicksNearest) {
   std::vector<std::shared_ptr<IObject>> objects;
   objects.push_back(std::make_shared<SphereFixture>(Vector3D(0, 0, -5), 0.4));
@@ -72,7 +68,6 @@ TEST(BVHTest, ManyObjects_PicksNearest) {
   EXPECT_NEAR(rec.t, 0.6, 1e-6);
 }
 
-// Ray misses all five objects
 TEST(BVHTest, ManyObjects_Miss) {
   std::vector<std::shared_ptr<IObject>> objects;
   objects.push_back(std::make_shared<SphereFixture>(Vector3D(3, 0, -1), 0.4));
@@ -86,7 +81,6 @@ TEST(BVHTest, ManyObjects_Miss) {
   EXPECT_FALSE(bvh.hits(ray, 0.001, 1000.0, rec));
 }
 
-// getBoundingBox encompasses all children
 TEST(BVHTest, BoundingBoxEncompassesAll) {
   std::vector<std::shared_ptr<IObject>> objects;
   objects.push_back(std::make_shared<SphereFixture>(Vector3D(-3, 0, 0), 0.5));
@@ -99,7 +93,6 @@ TEST(BVHTest, BoundingBoxEncompassesAll) {
   EXPECT_GE(box.max.x, 3.0);
 }
 
-// Objects spread on X axis: split should still find the right hit
 TEST(BVHTest, ObjectsAlongX_PicksCorrectHit) {
   std::vector<std::shared_ptr<IObject>> objects;
   for (int xOffset = -2; xOffset <= 2; ++xOffset) {
@@ -109,7 +102,6 @@ TEST(BVHTest, ObjectsAlongX_PicksCorrectHit) {
 
   BVHNode bvh(std::move(objects));
 
-  // Ray aimed at the sphere centred on x=0
   Ray ray(Vector3D(0, 0, 0), Vector3D(0, 0, -1));
   HitRecord rec;
   EXPECT_TRUE(bvh.hits(ray, 0.001, 1000.0, rec));
