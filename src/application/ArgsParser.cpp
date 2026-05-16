e444f6fdc453af5fd72573c9e0c138cfe0dbda95
LoannBi <loann.bideau@epitech.eu>
2026-05-16 23:33:37 +0200

    fix(args): return nullopt for unknown flags to restore strict validation

--- src/application/ArgsParser.cpp
+++ src/application/ArgsParser.cpp
@@ -10,28 +10,6 @@
 
 namespace raytracer::core {
 
-namespace {
-
-bool extractFlag(int argc, const char** argv, std::string_view flag,
-                 std::string& outValue) {
-  for (int i = 1; i < argc - 1; ++i) {
-    if (std::string_view(argv[i]) == flag) {
-      outValue = argv[i + 1];
-      return true;
-    }
-  }
-  return false;
-}
-
-bool hasFlag(int argc, const char** argv, std::string_view flag) {
-  for (int i = 1; i < argc; ++i) {
-    if (std::string_view(argv[i]) == flag) return true;
-  }
-  return false;
-}
-
-}  // namespace
-
 std::optional<AppConfig> ArgsParser::parse(int argc, const char** argv) {
   if (argc == 2 && (std::string_view(argv[1]) == "-h" ||
                     std::string_view(argv[1]) == "--help")) {
@@ -43,13 +21,21 @@ std::optional<AppConfig> ArgsParser::parse(int argc, const char** argv) {
 
   SceneRequest request;
   request.scenePath = argv[1];
-  request.useBVH = !hasFlag(argc, argv, "--no-bvh");
+  request.useBVH = true;
 
-  std::string configPath;
-  if (extractFlag(argc, argv, "--config", configPath)) {
-    request.renderConfigPath = configPath;
+  int i = 2;
+  while (i < argc) {
+    const std::string_view arg(argv[i]);
+    if (arg == "--no-bvh") {
+      request.useBVH = false;
+      ++i;
+    } else if (arg == "--config" && i + 1 < argc) {
+      request.renderConfigPath = argv[i + 1];
+      i += 2;
+    } else {
+      return std::nullopt;
+    }
   }
-
   return request;
 }
 
